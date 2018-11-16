f0316d5b2d1d8c3229713e189074397fae256340
Raymond Li <it1011@qq.com>
2018-11-16 16:56:25 +0800

    feat(plugins): 添加styled-components转换的plguins

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # get-babel-config
 base babel configuration
+## 4.0.2
+添加styled-component plugins
--- index.js
+++ index.js
@@ -1,77 +1,95 @@
 // const envConst = require("../constants.js");
 const path = require("path");
 module.exports = function(options = {}) {
-    let {
-        targets: tgt,
-        transformInclude = [],
-        engines = ["react"],
-        loose = true,
-        modules = false
-    } = options;
+  let {
+    targets: tgt,
+    transformInclude = [],
+    engines = ["react"],
+    loose = true,
+    modules = false
+  } = options;
 
-    let strDev = "development";
-    const env = process.env.NODE_ENV || "development";
+  let strDev = "development";
+  let strProd = "production";
+  const env = process.env.NODE_ENV || strDev;
 
-    let isDev = env === strDev;
+  let isProd = env === strProd;
 
-    // const userHasDefinedTargets = !!(targetBrowsers || tgt);
+  // const userHasDefinedTargets = !!(targetBrowsers || tgt);
 
-    // // 如果没有传递目标浏览器，则配置支持使用chrome > 58版本，减少plugins和polyfills的数量
-    // targets = tgt || {
-    //     browsers: targetBrowsers || "chrome >= 58"
-    // };
-    // //如果配置了targets， 测不使用内置plugins
-    // const innerPlugins = userHasDefinedTargets
-    //     ? []
-    //     : require("./data/plugins.json");
-    // // 在innerPlugins和用户配置的include中去掉重复的部分。
-    // let includeFeature = require("lodash.uniq")([
-    //     ...innerPlugins,
-    //     ...transformInclude
-    // ]);
-    targets = tgt || {
-        chrome: '45',
-        edge: '14',
-        ie: '10'
-    }
+  // // 如果没有传递目标浏览器，则配置支持使用chrome > 58版本，减少plugins和polyfills的数量
+  // targets = tgt || {
+  //     browsers: targetBrowsers || "chrome >= 58"
+  // };
+  // //如果配置了targets， 测不使用内置plugins
+  // const innerPlugins = userHasDefinedTargets
+  //     ? []
+  //     : require("./data/plugins.json");
+  // // 在innerPlugins和用户配置的include中去掉重复的部分。
+  // let includeFeature = require("lodash.uniq")([
+  //     ...innerPlugins,
+  //     ...transformInclude
+  // ]);
+  targets = tgt || {
+    chrome: "45",
+    edge: "14",
+    ie: "10"
+  };
 
-    let presets = [
-            [
-                "@babel/preset-env",
-                {
-                    targets,
-                    modules: env === "test" ? "commonjs" : modules,
-                    include: transformInclude,
-                    useBuiltIns: "usage",
-                    debug: isDev,
-                    loose
-                }
-            ]
-        ],
-        plugins = [
-            "@babel/plugin-syntax-dynamic-import",
-            ["@babel/plugin-proposal-decorators", {
-                legacy: true
-            }],
-            ["@babel/plugin-proposal-class-properties", {
-                loose
-            }],
-            "@babel/plugin-transform-runtime"
-        ];
+  let presets = [
+      [
+        "@babel/preset-env",
+        {
+          targets,
+          modules: env === "test" ? "commonjs" : modules,
+          include: transformInclude,
+          useBuiltIns: "usage",
+          debug: !isProd,
+          loose
+        }
+      ]
+    ],
+    plugins = [
+      "@babel/plugin-syntax-dynamic-import",
+      [
+        "@babel/plugin-proposal-decorators",
+        {
+          legacy: true
+        }
+      ],
+      [
+        "@babel/plugin-proposal-class-properties",
+        {
+          loose
+        }
+      ],
+      "@babel/plugin-transform-runtime"
+    ];
 
-    let hasReact = engines.indexOf("react") !== -1;
+  let hasReact = engines.indexOf("react") !== -1;
 
-    if (hasReact) {
-        presets.push([
-            "@babel/preset-react",
-            {
-                development: isDev
-            }
-        ]);
-    }
+  if (hasReact) {
+    plugins = [
+      [
+        "babel-plugin-styled-components",
+        {
+          ssr: false,
+          displayName: !isProd,
+          fileName: !isProd
+        }
+      ],
+      ...plugins
+    ];
+    presets.push([
+      "@babel/preset-react",
+      {
+        development: !isProd
+      }
+    ]);
+  }
 
-    return {
-        presets,
-        plugins
-    };
+  return {
+    presets,
+    plugins
+  };
 };
